0642428bee88794b15cd48236f81835972a4d6f7
jingjok-g <58161624+jingjok-g@users.noreply.github.com>
2021-09-15 12:25:38 +0700

    format

--- if-else.php
+++ if-else.php
@@ -1,12 +1,12 @@
 easy way to execute conditional html / javascript / css / other language code with php if else:
 
-<?php if (condition): ?>
+<?php if (condition) : ?>
 
-html code to run if condition is true
+    html code to run if condition is true
 
-<?php else: ?>
+<?php else : ?>
 
-html code to run if condition is false
+    html code to run if condition is false
 
 <?php endif ?>
 
@@ -29,20 +29,20 @@ if ($a > $b) {
 <?php
 
 /* Incorrect Method: */
-if ($a > $b):
-    echo $a." is greater than ".$b;
-else if ($a == $b): // Will not compile.
+if ($a > $b) :
+    echo $a . " is greater than " . $b;
+else if ($a == $b) : // Will not compile.
     echo "The above line causes a parse error.";
 endif;
 
 
 /* Correct Method: */
-if ($a > $b):
-    echo $a." is greater than ".$b;
-elseif ($a == $b): // Note the combination of the words.
-    echo $a." equals ".$b;
-else:
-    echo $a." is neither greater than or equal to ".$b;
+if ($a > $b) :
+    echo $a . " is greater than " . $b;
+elseif ($a == $b) : // Note the combination of the words.
+    echo $a . " equals " . $b;
+else :
+    echo $a . " is neither greater than or equal to " . $b;
 endif;
 
 ?>
