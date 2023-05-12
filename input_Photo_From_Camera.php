f9eff1c0e775e0325016c0c78c1a98a7ae12d3af
jingjok-g <58161624+jingjok-g@users.noreply.github.com>
2023-05-12 13:30:48 +0700

    ถ่ายรูปจากกล้องหน้าเก็บลงเครื่อง

---
+++ input_Photo_From_Camera.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ถ่ายรูปจากกล้องหน้าเก็บลงเครื่อง</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <style type="text/css">
+        button {
+            width: 120px;
+            padding: 10px;
+            display: block;
+            margin: 20px auto;
+            border: 2px solid #111111;
+            cursor: pointer;
+            background-color: white;
+        }
+
+        #start_camera {
+            margin-top: 50px;
+        }
+
+        #video {
+            display: none;
+            margin: 50px auto 0 auto;
+        }
+
+        #click_photo {
+            display: none;
+        }
+
+        #dataurl-container {
+            display: none;
+        }
+
+        #canvas {
+            display: block;
+            margin: 0 auto 20px auto;
+        }
+
+        #dataurl-header {
+            text-align: center;
+            font-size: 15px;
+        }
+
+        #dataurl {
+            display: block;
+            height: 100px;
+            width: 320px;
+            margin: 10px auto;
+            resize: none;
+            outline: none;
+            border: 1px solid #111111;
+            padding: 5px;
+            font-size: 13px;
+            box-sizing: border-box;
+        }
+    </style>
+</head>
+
+<body>
+
+    <button id="start_camera">Start Camera</button>
+    <video id="video" width="320" height="240" autoplay></video>
+    <button id="click_photo">Click Photo</button>
+    <div id="dataurl-container">
+        <form method="post">
+            <canvas id="canvas" width="320" height="240"></canvas>
+            <div id="dataurl-header">Image Data URL</div>
+            <textarea id="dataurl" name="data_url" readonly></textarea>
+            <center><input id="savephoto" type="submit" value="save img"></center>
+        </form>
+    </div>
+
+    <script>
+        let camera_button = document.querySelector("#start_camera");
+        let video = document.querySelector("#video");
+        let click_button = document.querySelector("#click_photo");
+        let canvas = document.querySelector("#canvas");
+        let dataurl = document.querySelector("#dataurl");
+        let dataurl_container = document.querySelector("#dataurl-container");
+
+        camera_button.addEventListener('click', async function() {
+            let stream = null;
+
+            try {
+                stream = await navigator.mediaDevices.getUserMedia({
+                    video: true,
+                    audio: false
+                });
+            } catch (error) {
+                alert(error.message);
+                return;
+            }
+
+            video.srcObject = stream;
+
+            video.style.display = 'block';
+            camera_button.style.display = 'none';
+            click_button.style.display = 'block';
+
+            // ถ่ายรูปอัตโนมัติ
+            setTimeout(function() {
+                click_photo.click();
+            }, 1000);
+        });
+
+        click_button.addEventListener('click', function() {
+            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+            let image_data_url = canvas.toDataURL('image/jpeg');
+
+            dataurl.value = image_data_url;
+            dataurl_container.style.display = 'block';
+
+            // save รูปอัตโนมัติ
+            setTimeout(function() {
+                savephoto.click();
+            }, 1000);
+        });
+    </script>
+    <?php
+    $_POST['data_url'] = $_POST['data_url'] ?? '';
+    if ($_POST["data_url"] != '') {
+        // data url string that was uploaded
+        $data_url = $_POST["data_url"];
+
+        list($type, $data) = explode(';', $data_url);
+        list(, $data)      = explode(',', $data);
+        $data = base64_decode($data);
+
+        file_put_contents('z.jpg', $data);
+        header("Location: success.php");
+        exit();
+    }
+    ?>
+</body>
+
+</html>
